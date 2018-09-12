e3620e64074137fa28127d2527203d8e3a325c8e
Sebastian Thelen <sebastianthelen@gmail.com>
2018-09-12 17:10:22 +0200

    ready to compare models

--- deep_eurovoc.py
+++ deep_eurovoc.py
@@ -17,6 +17,11 @@ from keras.layers import Conv1D, MaxPooling1D, Embedding
 from keras.models import Model
 from keras.initializers import Constant
 from keras.layers import Dropout
+from keras import regularizers
+import keras.losses
+
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfb
 
 from sklearn.preprocessing import MultiLabelBinarizer
 
@@ -27,33 +32,33 @@ MAX_NUM_WORDS = 20000 # max. size of vocabulary
 EMBEDDING_DIM = 100 # dimension of GloVe word embeddings
 MAX_SEQUENCE_LENGTH = 1000 # truncate examples after MAX_SEQUENCE_LENGTH words
 VALIDATION_SPLIT = 0.2 # ration for split of training data and test data
-NUM_EPOCHS = 50 # number of epochs the network is trained
-EUROVOC_FIELDS = {  # those are the eurovoc fields we want to support
-    "4":  "POLITICS",
-    "8":  "INTERNATIONAL RELATIONS", 
-    "10": "EUROPEAN UNION",
-    "12": "LAW ",
-    "16": "ECONOMICS",
-    "20": "TRADE",
-    "24": "FINANCE",
-    "28": "SOCIAL QUESTIONS",
-    "32": "EDUCATION AND COMMUNICATIONS",
-    "36": "SCIENCE",
-    "40": "BUSINESS AND COMPETITION",
-    "44": "EMPLOYMENT AND WORKING CONDITIONS",
-    "48": "TRANSPORT",
-    "52": "ENVIRONMENT",
-    "56": "AGRICULTURE, FORESTRY AND FISHERIES",
-    "60": "AGRI-FOODSTUFFS",
-    "64": "PRODUCTION, TECHNOLOGY AND RESEARCH",
-    "66": "ENERGY",
-    "68": "INDUSTRY",
-    "72": "GEOGRAPHY",
-    "76": "INTERNATIONAL ORGANISATIONS"
+NUM_EPOCHS = 10 # number of epochs the network is trained
+# those are the eurovoc fields we want to support
+EUROVOC_FIELDS = {
+"4":  "POLITICS",
+"8":  "INTERNATIONAL RELATIONS", 
+"10": "EUROPEAN UNION",
+"12": "LAW ",
+"16": "ECONOMICS",
+"20": "TRADE",
+"24": "FINANCE",
+"28": "SOCIAL QUESTIONS",
+"32": "EDUCATION AND COMMUNICATIONS",
+"36": "SCIENCE",
+"40": "BUSINESS AND COMPETITION",
+"44": "EMPLOYMENT AND WORKING CONDITIONS",
+"48": "TRANSPORT",
+"52": "ENVIRONMENT",
+"56": "AGRICULTURE, FORESTRY AND FISHERIES",
+"60": "AGRI-FOODSTUFFS",
+"64": "PRODUCTION, TECHNOLOGY AND RESEARCH",
+"66": "ENERGY",
+"68": "INDUSTRY",
+"72": "GEOGRAPHY",
+"76": "INTERNATIONAL ORGANISATIONS"
 }
 
 data_df = pd.read_csv("data.csv")
-print(data_df.info())
 
 def cleanup_abstract(xmlstring):
     #import ipdb; ipdb.set_trace()
@@ -66,18 +71,20 @@ def cleanup_abstract(xmlstring):
     except:
         text = xmlstring
     # remove stopwords and punctuation. lower case everything
-    stop_words = set(stopwords.words('english'))
-    tokens = word_tokenize(text)
-    tokens = [w.lower() for w in tokens if not w in stop_words and w.isalpha() and wordnet.synsets(w)]
+    #stop_words = set(stopwords.words('english'))
+    #tokens = word_tokenize(text)
+    #tokens = [w.lower() for w in tokens if not w in stop_words and w.isalpha() and wordnet.synsets(w)]
     # lemmatize
-    lemma = WordNetLemmatizer()
-    final_tokens = []
-    for word in tokens:
-        final_tokens.append(lemma.lemmatize(word))
-    ret = " ".join(final_tokens)
-    return ret
-    
- if CLEANUP_DATA:
+    #lemma = WordNetLemmatizer()
+    #final_tokens = []
+    #for word in tokens:
+    #    final_tokens.append(lemma.lemmatize(word))
+    #ret = " ".join(final_tokens)
+    #return ret
+    return text
+
+
+if CLEANUP_DATA:
     data_df["clean_abstract"] = data_df["abstract"].apply(cleanup_abstract)
     data_df["clean_concepts"] = data_df["concepts"].apply(lambda x: list({c[c.rfind("/")+1:c.rfind("/")+3] for c in x.split(";") if c[c.rfind("/")+1:c.rfind("/")+3] in EUROVOC_FIELDS}))
     data_df = data_df[data_df.astype(str)['clean_concepts'] != '[]']
@@ -86,44 +93,17 @@ def cleanup_abstract(xmlstring):
     data_df.to_pickle("data_df.pkl")
     
 data_df = pd.read_pickle("data_df.pkl")
-print(data_df['clean_abstract'][:5])
-print(data_df['clean_concepts'][:5])
 
 labels = data_df["clean_concepts"].tolist()
 mlb = MultiLabelBinarizer()
 labels = mlb.fit_transform(labels)
-# loop over each of the possible class labels and show them
-for (i, label) in enumerate(mlb.classes_):
-	print("{}. {}".format(i + 1, label))
-    
-print("Labels of the 2nd training example: " + str(mlb.inverse_transform(np.array([labels[1]]))))
 
 data = data_df["clean_abstract"].tolist()
 tokenizer = Tokenizer(nb_words=MAX_NUM_WORDS)
 tokenizer.fit_on_texts(data)
 sequences = tokenizer.texts_to_sequences(data)
 word_index = tokenizer.word_index
-print('Found %s unique tokens.' % len(word_index))
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
-print('Shape of data tensor:', data.shape)
-print('Shape of label tensor:', labels.shape)
-
-# split the data into a training set and a validation set
-indices = np.arange(data.shape[0])
-np.random.shuffle(indices)
-data = data[indices]
-labels = labels[indices]
-num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
-print(num_validation_samples)
-trainX = data[:-num_validation_samples]
-trainY = labels[:-num_validation_samples]
-testX = data[-num_validation_samples:]
-testY = labels[-num_validation_samples:]
-
-print("trainX.shape", trainX.shape)
-print("trainY.shape", trainY.shape)
-print("testX.shape", testX.shape)
-print("testY.shape", testY.shape)
 
 embeddings_index = {}
 with open(os.path.join('glove.6B', 'glove.6B.100d.txt'), 'r', encoding='utf-8') as f:
@@ -132,9 +112,7 @@ with open(os.path.join('glove.6B', 'glove.6B.100d.txt'), 'r', encoding='utf-8') 
         word = values[0]
         coefs = np.asarray(values[1:], dtype='float32')
         embeddings_index[word] = coefs
-
-print('Found %s word vectors.' % len(embeddings_index))
-
+        
 num_words = min(MAX_NUM_WORDS, len(word_index) + 1)
 embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
 for word, i in word_index.items():
@@ -152,30 +130,79 @@ embedding_layer = Embedding(num_words,
                             embeddings_initializer=Constant(embedding_matrix),
                             input_length=MAX_SEQUENCE_LENGTH,
                             trainable=False)
-                            
-sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-embedded_sequences = embedding_layer(sequence_input)
-x = Conv1D(128, 5, activation='relu')(embedded_sequences)
-x = MaxPooling1D(5)(x)
-x = Dropout(0.25)(x)
-x = Conv1D(128, 5, activation='relu')(x)
-x = MaxPooling1D(5)(x)
-x = Dropout(0.25)(x)
-x = Conv1D(128, 5, activation='relu')(x)
-x = MaxPooling1D(35)(x)  # global max pooling
-x = Dropout(0.25)(x)
-x = Flatten()(x)
-x = Dense(128, activation='relu')(x)
-preds = Dense(labels.shape[1], activation='sigmoid')(x)
 
-model = Model(sequence_input, preds)
-model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['categorical_accuracy'])
-              
-print(model.summary())
+for POS_WEIGHT in [1, 5, 10]:
+    for DROPOUT in [0.1, 0.25, 0.5]:
+        for REGULARIZATION in [0.001, 0.01, 0.1]:
+            for BATCH_SIZE in [256]:
+                
+                params ={"pos_weight": str(POS_WEIGHT), 
+                          "dropout": str(DROPOUT), 
+                          "regularization": str(REGULARIZATION), 
+                          "batch_size": str(BATCH_SIZE)}
+                
+                def weighted_binary_crossentropy(target, output):
+                    """
+                    Weighted binary crossentropy between an output tensor 
+                    and a target tensor. POS_WEIGHT is used as a multiplier 
+                    for the positive targets.
 
-history = model.fit(trainX, trainY, validation_data=(testX, testY),
-          epochs=NUM_EPOCHS, batch_size=128)
-          
-model.save("model.h5")
+                    Combination of the following functions:
+                    * keras.losses.binary_crossentropy
+                    * keras.backend.tensorflow_backend.binary_crossentropy
+                    * tf.nn.weighted_cross_entropy_with_logits
+                    """
+                    # transform back to logits
+                    _epsilon = tfb._to_tensor(tfb.epsilon(), output.dtype.base_dtype)
+                    output = tf.clip_by_value(output, _epsilon, 1 - _epsilon)
+                    output = tf.log(output / (1 - output))
+                    # compute weighted loss
+                    loss = tf.nn.weighted_cross_entropy_with_logits(targets=target,
+                                                                    logits=output,
+                                                                    pos_weight=POS_WEIGHT)
+                    return tf.reduce_mean(loss, axis=-1)
+
+                sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+                embedded_sequences = embedding_layer(sequence_input)
+                x = Conv1D(128, 5, activation='relu', kernel_regularizer=regularizers.l2(REGULARIZATION))(embedded_sequences)
+                x = MaxPooling1D(5)(x)
+                x = Conv1D(128, 5, activation='relu', kernel_regularizer=regularizers.l2(REGULARIZATION))(x)
+                x = MaxPooling1D(5)(x)
+                x = Conv1D(128, 5, activation='relu', kernel_regularizer=regularizers.l2(REGULARIZATION))(x)
+                x = MaxPooling1D(35)(x)  # global max pooling
+                x = Flatten()(x)
+                x = Dropout(DROPOUT)(x)
+                x = Dense(128, activation='relu',kernel_regularizer=regularizers.l2(REGULARIZATION))(x)
+                x = Dropout(DROPOUT)(x)
+                preds = Dense(labels.shape[1], activation='sigmoid', kernel_regularizer=regularizers.l2(REGULARIZATION))(x)
+
+                keras.losses.weighted_binary_crossentropy = weighted_binary_crossentropy
+
+                model = Model(sequence_input, preds)
+                model.compile(loss='weighted_binary_crossentropy',
+                              optimizer='adam',
+                              metrics=['categorical_accuracy'])
+                history = model.fit(data, labels, validation_split=0.2,
+                          epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)          
+                model.save("models/model_%(pos_weight)s_%(dropout)s_%(regularization)s_%(batch_size)s.h5"%params)
+                
+                acc = history.history['categorical_accuracy']
+                val_acc = history.history['val_categorical_accuracy']
+                loss = history.history['loss']
+                val_loss = history.history['val_loss']
+                epochs = range(len(acc))
+                fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+                ax1.plot(epochs, acc, label='Training acc')
+                ax1.plot(epochs, val_acc, label='Validation acc')
+                ax1.set_ylabel('accuracy')
+                ax1.set_xlabel('epoch')
+                ax1.set_title('Accuracy: PW %(pos_weight)s, DO %(dropout)s, REG %(regularization)s, BATCH %(batch_size)s'% params)
+                ax1.legend()
+                ax2.plot(epochs, loss, label='Training loss')
+                ax2.plot(epochs, val_loss, label='Validation loss')
+                ax2.set_ylabel('loss')
+                ax2.set_xlabel('epoch')
+                ax2.set_title('Loss: PW %(pos_weight)s, DO %(dropout)s, REG %(regularization)s, BATCH %(batch_size)s'%params)
+                ax2.legend()
+                fig.savefig("figures/model_%(pos_weight)s_%(dropout)s_%(regularization)s_%(batch_size)s.png"%params)
+                
